bb0c4dea72b84208179392757f9115f1a3d8b27d
carlos.avellar <carlos.avellar@gmail.com>
2022-10-26 18:03:58 -0300

    corrections

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import { CharacterList } from './CharacterList';
-import { getCharacterRequest, getCharacterSuccess, Types } from './../actions/character';
+import { getCharacterRequest, getCharacterSuccess } from './../actions/character';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import imgHeader from './../assets/images/header.jpg';
@@ -13,13 +13,13 @@ function App(props) {
 
   useEffect(() => {
     props.getCharacterRequest();
-  }, []);
+  }, [props]);
 
-  const loadChars = () => {
+  const loadChars = useCallback(() => {
     if (chars.length === 0) {
       setChars(props.character.items);
     }
-  };
+  }, [chars, props.character]);
 
   useEffect(() => {
     loadChars();
@@ -40,7 +40,7 @@ function App(props) {
       <Container container spacing={2}>
         <Grid>
           <header className="App-header">
-            <img src={imgHeader} />
+            <img src={imgHeader} alt="Header" />
           </header>
           <Grid container rowSpacing={1} component="div">
             <SearchForm container onSearchChar={handlerFilterCharacter} />
